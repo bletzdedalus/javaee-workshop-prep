7729fe4c02a3d85b1702247297d8c31709bcd6b8
cleminem <clemens,neuwirth@gmx.net>
2023-06-21 10:47:17 +0200

    sql statement

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,5 +1,5 @@
 -- This file allow to write SQL commands that will be emitted in test and dev.
 -- The commands are commented as their support depends of the database
--- insert into myentity (id, field) values(nextval('hibernate_sequence'), 'field-1');
--- insert into myentity (id, field) values(nextval('hibernate_sequence'), 'field-2');
--- insert into myentity (id, field) values(nextval('hibernate_sequence'), 'field-3');
+insert into Animal (id, name, type, comment, available) values(nextval('hibernate_sequence'), 'Chloe', "Fish", "boring", "true");
+insert into myentity (id, field) values(nextval('hibernate_sequence'), 'field-2');
+insert into myentity (id, field) values(nextval('hibernate_sequence'), 'field-3');
